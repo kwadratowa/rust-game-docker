7c0eae6f405c510fd15231e72a19f093d331aa16
Mihoko Okayami <mihoko.okayami@pm.me>
2021-07-09 19:39:31 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/ash
-docker buildx build --platform=linux/amd64 --push -t mihokookayami/rust:update-server . -f update-server.Dockerfile
-docker buildx build --platform=linux/amd64 --push -t mihokookayami/rust:update-oxide . -f update-oxide.Dockerfile
-docker buildx build --platform=linux/amd64 --push -t mihokookayami/rust:launcher . -f launcher.Dockerfile
