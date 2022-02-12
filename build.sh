5803c2a6c8a8c2c25e5b5d565feb66f21d6802d7
Artur Motyka <artur@motyka.pro>
2022-02-13 00:23:43 +0100

    Add support for custom maps

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker buildx build -t artur9010/rust:update-server -f update-server.Dockerfile 
 docker push artur9010/rust:update-server
 docker buildx build -t artur9010/rust:update-oxide -f update-oxide.Dockerfile --platform linux/amd64 .
 docker push artur9010/rust:update-oxide
-docker buildx build -t artur9010/rust:launcher -f launcher.Dockerfile --platform linux/amd64 .
+docker buildx build -t artur9010/rust:launcher-custom-map -f launcher.Dockerfile --platform linux/amd64 .
 docker push artur9010/rust:launcher
